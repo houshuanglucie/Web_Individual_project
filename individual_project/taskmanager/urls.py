067a30dbb24449829c08247d3e70c9025c4e067a
houshuanglucie <houshuang960330@163.com>
2020-04-30 22:34:45 +0200

    Implement all functionality (without css)

--- individual_project/taskmanager/urls.py
+++ individual_project/taskmanager/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # path('projects/', views.connection, name='projects'),
     path('project/<int:project_id>/', views.project, name='project'),
     path('task/<int:task_id>/', views.task, name='task'),
-    path('add_task/', views.add_task, name='add_task'),
     path('add_journal/', views.add_journal, name='add_journal'),
+    path('add_task/', views.add_task, name='add_task'),
+    path('edit_task/<int:task_id>/', views.edit_task, name='edit_task'),
+    # path('del_task/<int:task_id>/', views.del_task, name='del_task'),
 ]
--- individual_project/taskmanager/views.py
+++ individual_project/taskmanager/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
 # from django.http import HttpResponseRedirect, Http404
 # from django.contrib.auth.decorators import login_required
-from .forms import ConnectionForm, JournalForm, TaskForm
+from .forms import ConnectionForm, JournalForm, TaskForm, EditTaskForm
 from .models import Project, Task, Journal
 
 
@@ -58,10 +58,10 @@ def add_journal(request):
     if request.method != 'POST':
         form = JournalForm()
     else:
-        form = JournalForm(request.POST)
-        if form.is_valid():
-            form.save()
-            task_get = form.cleaned_data['task']
+        new_journal = JournalForm(request.POST)
+        if new_journal.is_valid():
+            new_journal.save()
+            task_get = new_journal.cleaned_data['task']
             journal_list = Journal.objects.filter(task=task_get)
             return render(request, 'taskmanager/task_detail.html', locals())
     return render(request, 'taskmanager/add_journal.html', locals())
@@ -69,6 +69,7 @@ def add_journal(request):
 
 # Add a new task of a project
 def add_task(request):
+    edit = False
     if request.method != 'POST':
         form = TaskForm()
     else:
@@ -80,4 +81,34 @@ def add_task(request):
             return render(request, 'taskmanager/project.html', locals())
     return render(request, 'taskmanager/add_task.html', locals())
 
+
+# Edit a task
+def edit_task(request, task_id):
+    edit = True
+    task_before = Task.objects.get(id=task_id)
+    if request.method != 'POST':
+        form = EditTaskForm(instance=task_before)
+    else:
+        form = EditTaskForm(data=request.POST, instance=task_before)
+        if form.is_valid():
+            form.save()
+            project_get = form.cleaned_data['project']
+            task_list = Task.objects.filter(project=project_get)
+            return render(request, 'taskmanager/project.html', locals())
+            # return redirect('app1:article')
+    context = {'form': form, 'task': task_before, 'edit': edit}
+    return render(request, 'taskmanager/add_task.html', context)
+
+
+# Delete a task
+# def del_task(request, task_id):
+#     delete = True
+#     task_current = Task.objects.get(id=task_id)
+#     if request.method == 'POST':
+#         project_get = task_current.cleaned_data['project']
+#         task_current.delete()
+#         task_list = Task.objects.filter(project=project_get)
+#         return render(request, 'taskmanager/project.html', locals())
+#         # return redirect('app1:article')
+#     return render(request, 'taskmanager/add_task.html', locals())
 
